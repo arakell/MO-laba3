751613c61f8aadc92dcb6b41eb10123390d4a7c9
Arina <arina.safronova@inbox.ru>
2024-03-12 12:57:17 +0700

    Добавлен метод Бройдена и Пирсона, Пирсон не работает

---
+++ 2laba.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+#include "libxl.h"
+#include "GradientMethod.cpp"
+#include "ConjugateGradMethod.cpp"
+#include "Functions.h"
+#include "Broiden.cpp"
+#include "SecondPirson.cpp"
+using namespace libxl;
+using namespace std;
+
+
+
+int main()
+{
+
+    Book* book = xlCreateXMLBook(); // xlCreateXMLBook() for xlsx
+    Format* format = book->addFormat();
+    format->setNumFormat(book->addCustomNumFormat(L"0.00000"));
+    Sheet* sheet;
+    vector<double> X1 = { 0, 0 };
+    vector<double> X2 = { -1.2, 1 };
+    
+    
+    sheet = book->addSheet(L"Broiden1");
+    Broiden* b1 = new Broiden(book, sheet, format, X1, f1, gradf1);
+    b1->method();
+
+    sheet = book->addSheet(L"Broiden2");
+    Broiden* b2 = new Broiden(book, sheet, format, X2, f2, gradf2);
+    b2->method();
+    
+
+    
+    sheet = book->addSheet(L"SecondPirson1");
+    SecondPirson* Sp1 = new SecondPirson(book, sheet, format, X1, f1, gradf1);
+    Sp1->method();
+    
+    sheet = book->addSheet(L"SecondPirson2");
+    SecondPirson* Sp2 = new SecondPirson(book, sheet, format, X2, f2, gradf2);
+    Sp2->method();
+    
+    book->save(L"laba2.xlsx");
+    book->release();
+    return 0;
+}
---
+++ SecondPirson.cpp
@@ -0,0 +1,145 @@
+#include <vector>
+#include "libxl.h"
+#include <iostream>
+using namespace std;
+using namespace libxl;
+typedef double (*Function)(vector<double>);
+typedef void (*Gradient)(vector<double>, vector<double>&);
+
+
+class SecondPirson {
+public:
+
+	Book* book;
+	Sheet* sheet;
+	Format* format;
+	int row = 4;
+
+	vector<double> X = { 0, 0 };
+	vector<double> X_new = { 0, 0 };
+	vector<double> deltaX = { 0, 0 };
+
+	Gradient grad;
+	vector<double> gradX = { 0, 0 };
+	vector<double> gradX_new = { 0, 0 };
+	vector<double> deltaG = { 0, 0 };
+
+	Function f;
+	vector<vector <double>> N = { {1, 0}, {0, 1} };
+
+	vector<double> temp = { 0, 0 };
+
+	vector<vector <double>> divisor = { {1, 0}, {0, 1} };
+
+
+	double eps = 0.01;
+
+	SecondPirson(Book* book, Sheet* sheet, Format* format, vector<double> X, Function f, Gradient grad) {
+
+		this->book = book;
+		this->sheet = sheet;
+		this->format = format;
+		this->X = X;
+		this->f = f;
+		this->grad = grad;
+		sheet->writeStr(row, 3, L"x");
+		sheet->writeStr(row, 4, L"f");
+		sheet->writeStr(row, 5, L"grad");
+		sheet->writeStr(row, 6, L"N");
+		sheet->writeStr(row, 8, L"lambda");
+		sheet->writeStr(row, 9, L"x_new");
+		sheet->writeStr(row, 10, L"f_new");
+		sheet->writeStr(row, 11, L"N_new");
+		row++;
+	}
+
+	void method() {
+
+		//cout << X[0] << " " << X[1] << endl;
+
+		do {
+			sheet->writeNum(row, 3, X[0], format);
+			sheet->writeNum(row + 1, 3, X[1], format);
+			sheet->writeNum(row, 4, f(X), format);
+
+			double lambda = searchLambda();
+			grad(X, gradX);
+			X_new[0] = X[0] - lambda * (N[0][0] * gradX[0] + N[0][1] * gradX[1]);
+			X_new[1] = X[1] - lambda * (N[1][0] * gradX[0] + N[1][1] * gradX[1]);
+			grad(X_new, gradX_new);
+
+			sheet->writeNum(row, 5, gradX[0], format);
+			sheet->writeNum(row + 1, 5, gradX[1], format);
+
+			sheet->writeNum(row, 6, N[0][0], format);
+			sheet->writeNum(row, 7, N[0][1], format);
+			sheet->writeNum(row + 1, 6, N[1][0], format);
+			sheet->writeNum(row + 1, 7, N[1][1], format);
+
+			sheet->writeNum(row, 8, lambda, format);
+
+			sheet->writeNum(row, 9, X_new[0], format);
+			sheet->writeNum(row + 1, 9, X_new[1], format);
+
+			sheet->writeNum(row, 10, f(X_new), format);
+
+			deltaX[0] = X_new[0] - X[0];
+			deltaX[1] = X_new[1] - X[1];
+			deltaG[0] = gradX_new[0] - gradX[0];
+			deltaG[1] = gradX_new[1] - gradX[1];
+
+			temp[0] = deltaX[0] - (N[0][0] * deltaG[0] + N[0][1] * deltaG[1]);
+			temp[1] = deltaX[1] - (N[1][0] * deltaG[0] + N[1][1] * deltaG[1]);
+
+			double m = deltaX[0] * deltaG[0] + deltaX[1] * deltaG[1];
+			cout << m << endl;
+
+			N[0][0] += temp[0] * deltaX[0] / m;
+			N[0][1] += temp[0] * deltaX[1] / m;
+			N[1][0] += temp[1] * deltaX[0] / m;
+			N[1][1] += temp[1] * deltaX[1] / m;
+
+			sheet->writeNum(row, 11, N[0][0], format);
+			sheet->writeNum(row, 12, N[0][1], format);
+			sheet->writeNum(row + 1, 11, N[1][0], format);
+			sheet->writeNum(row + 1, 12, N[1][1], format);
+
+			X[0] = X_new[0];
+			X[1] = X_new[1];
+			cout << X[0] << " " << X[1] << endl;
+			row += 3;
+
+		} while (sqrt(gradX_new[0] * gradX_new[0] + gradX_new[1] * gradX_new[1]) >
+			eps);
+	}
+
+	double searchLambda() {
+		double check = f(X);
+		double lambda = 0;
+		double delta = 0.0001;
+		grad(X, gradX);
+		while (delta > 0.0000000001) {
+			if (f(vector<double>({ X[0] - (lambda + delta) * (N[0][0] * gradX[0] + N[0][1] * gradX[1]),
+				X[1] - (lambda + delta) * (N[1][0] * gradX[0] + N[1][1] * gradX[1]) })) < check) {
+				lambda += delta;
+				check = f(vector<double>({ X[0] - lambda * (N[0][0] * gradX[0] + N[0][1] * gradX[1]),
+										   X[1] - lambda * (N[1][0] * gradX[0] + N[1][1] * gradX[1]) }));
+				grad(vector<double>({ X[0] - lambda * (N[0][0] * gradX[0] + N[0][1] * gradX[1]),
+									  X[1] - lambda * (N[1][0] * gradX[0] + N[1][1] * gradX[1]) }), gradX);
+			}
+			else if (f(vector<double>({ X[0] - (lambda - delta) * (N[0][0] * gradX[0] + N[0][1] * gradX[1]),
+				X[1] - (lambda - delta) * (N[1][0] * gradX[0] + N[1][1] * gradX[1]) })) < check) {
+				lambda -= delta;
+				check = f(vector<double>({ X[0] - lambda * (N[0][0] * gradX[0] + N[0][1] * gradX[1]),
+										   X[1] - lambda * (N[1][0] * gradX[0] + N[1][1] * gradX[1]) }));
+				grad(vector<double>({ X[0] - lambda * (N[0][0] * gradX[0] + N[0][1] * gradX[1]),
+									  X[1] - lambda * (N[1][0] * gradX[0] + N[1][1] * gradX[1]) }), gradX);
+			}
+			else delta /= 10;
+		}
+
+		return lambda;
+	}
+
+
+};
